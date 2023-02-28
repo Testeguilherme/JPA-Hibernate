58ea852bc924fe7e58dd4147a9a48696f93767fe
unknown <creusodagama3@hotmail.com>
2023-02-28 16:27:12 -0300

    msg

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -3,30 +3,31 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 		
-		Produto celular = new Produto();
-		celular.setNome("Xiami Redmi");
-		celular.setDescricao("Verde");
-		celular.setPreco(new BigDecimal(800));
+		Categoria celulares = new Categoria("CELULARES");
 		
+		Produto celular = new Produto("Samsung", "Preto", new BigDecimal(1500), celulares);
 		
-		//o parâmetro é o nome no persistence-unit
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = JPAUtil.getEntityManager();
 		
-		ProdutoDao dao = new ProdutoDao(em);
+		ProdutoDao produtoDao = new ProdutoDao(em);
+		CategoriaDao categoriaDao = new CategoriaDao(em);
 		
 		em.getTransaction().begin();
-		dao.cadastrar(celular);
+		
+		categoriaDao.cadastrar(celulares);
+		produtoDao.cadastrar(celular);
+		
 		em.getTransaction().commit();
 		em.close();
 		
--- src/main/java/br/com/alura/loja/util/JPAUtil.java
+++ src/main/java/br/com/alura/loja/util/JPAUtil.java
@@ -1,11 +1,17 @@
 package br.com.alura.loja.util;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class JPAUtil {
-
+	
+	//o parâmetro é o nome no persistence-unit
 	private static final EntityManagerFactory FACTORY = Persistence.createEntityManagerFactory("loja");
+	
+	public static EntityManager getEntityManager() {
+		return FACTORY.createEntityManager();
+	}
 	
 	
 }
